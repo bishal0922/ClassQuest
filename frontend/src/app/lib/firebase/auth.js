e7875ebe91ec09a38e3435a44e73b8977b83f88a
Bishal Giri <bishal0922@gmail.com>
2024-10-03 23:02:53 -0500

    firebase auth documentation

--- frontend/src/app/lib/firebase/auth.js
+++ frontend/src/app/lib/firebase/auth.js
@@ -1,3 +1,16 @@
+/**
+ * This file handles authentication-related functions using Firebase.
+ * 
+ * We import the necessary functions from Firebase's authentication module.
+ * 
+ * - `auth`: This is our Firebase authentication instance, which we configured in `firebase-config.js`.
+ * - `createUserWithEmailAndPassword`: This function allows us to create a new user with an email and password.
+ * - `signInWithEmailAndPassword`: This function allows us to sign in an existing user with an email and password.
+ * - `onAuthStateChanged`: This function is used to listen for changes in the authentication state (e.g., when a user signs in or out).
+ * - `signOut`: This function allows us to sign out the currently signed-in user.
+ * 
+ * We alias `onAuthStateChanged` and `signOut` to `_onAuthStateChanged` and `_signOut` respectively to avoid naming conflicts with our own functions.
+ */
 import { auth } from './firebase-config';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged as _onAuthStateChanged, signOut as _signOut } from 'firebase/auth';
 
